c217b20def53a3a1b51a938a65bccad7331f897b
Ingo Fruend <git@ingofruend.net>
2022-07-21 22:16:10 +0200

    Release process

--- publish.sh
+++ publish.sh
@@ -2,10 +2,15 @@
 
 set -xe
 
+VERSION=$1
+
+SOURCEDIST=csvmodel-${VERSION}.tar.gz
+BINDIST=csvmodel-${VERSION}-py3-none-any.whl
+
 cram README.md
 
 python -m build
 
-twine check dist/*
+twine check dist/$SOURCEDIST dist/$BINDIST
 
-twine upload dist/*
+twine upload dist/$SOURCEDIST dist/$BINDIST
