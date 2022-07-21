cdc6f51960d576ef3b3b0bd97e83bc0a80d23265
Ingo Fruend <git@ingofruend.net>
2022-07-21 22:38:50 +0200

    Automatic version bumps

--- publish.sh
+++ publish.sh
@@ -2,7 +2,19 @@
 
 set -xe
 
-VERSION=$1
+cp setup.cfg setup.cfg_backup
+
+function cleanup () {
+  mv setup.cfg_backup setup.cfg
+}
+
+trap cleanup ERR
+
+# bump version
+sed -r 's/version = ([0-9]+)/echo "version = $((\1+1))"/e' setup.cfg > setup.cfg_new
+mv setup.cfg_new setup.cfg
+
+VERSION=$(awk '/version = [0-9]+/ { print $3 }' setup.cfg)
 
 SOURCEDIST=csvmodel-${VERSION}.tar.gz
 BINDIST=csvmodel-${VERSION}-py3-none-any.whl
