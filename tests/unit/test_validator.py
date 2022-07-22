d112ef54ffcbd11efaaa448fac157ac94a7a32d7
Ingo Fruend <git@ingofruend.net>
2022-07-22 20:35:58 +0200

    Add test ~> already working

--- tests/unit/test_validator.py
+++ tests/unit/test_validator.py
@@ -312,3 +312,38 @@ class TestPydanticValidator:
                     details='csvmodel.testing:DoesNotExist',
                 )
             )
+
+    def test_pydantic_dataclasses(self, raw_csv):
+        with tempfile.TemporaryDirectory() as tdir:
+            fname = os.path.join(tdir, 'model.py')
+            with open(fname, 'w') as f:
+                f.write('\n'.join([
+                    'from pydantic.dataclasses import dataclass',
+                    '',
+                    '@dataclass',
+                    'class Model:',
+                    '    col1: str',
+                    '    col2: int',
+                    '    col3: float',
+                ]))
+
+            validator = PydanticValidator.from_schema(
+                SchemaSpec(
+                    type='file',
+                    details=':'.join([fname, 'Model'])
+                )
+            )
+
+        raw_csv.return_value = [
+            'col1,col2,col3',
+            'a,1,1',
+            'a,1.1,a',
+        ]
+
+        res = validator.check(CsvFile('any_file.csv'))
+
+        assert not res.ok
+        assert res.messages == [
+            'any_file.csv:3: Issue in column col2: value is not a valid integer',
+            'any_file.csv:3: Issue in column col3: value is not a valid float',
+        ]
